4ec25b2915bcbdb47bb8428f7adffda0b5fc8d03
terurin <hamusutaateru@live.jp>
2016-12-09 18:36:20 +0900

    dummy

---
+++ mdu_h28/Common/inc/packed_string.h
@@ -0,0 +1,18 @@
+/*
+ * CSString.h
+ *
+ *  Created on: 2016/12/02
+ *      Author: hamus
+ */
+
+#ifndef COMMON_CSSTRING_H_
+#define COMMON_CSSTRING_H_
+#include <string>
+#include <stdint.h>
+#include <stddef.h>
+#include <cstring>
+namespace common {
+
+}
+
+#endif /* COMMON_CSSTRING_H_ */
