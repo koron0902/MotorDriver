b616e8aa93e3fe6f5f93463391be476ab3cbf29a
関口 恵太 <koron0902@gmail.com>
2017-01-13 17:09:15 +0900

    制御モードスイッチ用関数の作成

---
+++ mdu_h28/Middle/inc/ControllerBase.hpp
@@ -0,0 +1,56 @@
+/*
+ * ControllerBase.h
+ *
+ *  Created on: 2016/12/11
+ *      Author: Keita
+ */
+
+#ifndef MIDDLE_INC_CONTROLLERBASE_HPP_
+#define MIDDLE_INC_CONTROLLERBASE_HPP_
+
+#include <fix.hpp>
+#include <unit.hpp>
+#include <functional>
+
+namespace Middle {
+	namespace Controller {
+
+		using namespace common;
+		class ControllerBase {
+		private:
+
+		protected:
+			fix32 mFreq;
+			static constexpr fix32 FREQ_DEFAULT = (30_KHz << fix32::shift);
+			static constexpr fix32 STEP_DEFAULT = (0.002 * fix32::gain);
+
+			std::function<void(void)> CallProc = nullptr;
+
+		public:
+			ControllerBase();
+			ControllerBase(const ControllerBase&) = default;
+			virtual ~ControllerBase();
+
+			uint32_t GetFreq(){
+				return (int32_t)mFreq;
+			}
+
+			void operator ()(void){
+				if(CallProc)
+					CallProc();
+			}
+
+		};
+
+		enum class ControlMode_e:uint32_t{
+			ModeTrapezium = 1,
+			ModePID = 2
+		};
+		extern ControlMode_e mMode;
+
+		void SwitchControlMode(ControlMode_e _mode);
+
+	} /* namespace Controller */
+} /* namespace Middle */
+
+#endif /* MIDDLE_INC_CONTROLLERBASE_HPP_ */
